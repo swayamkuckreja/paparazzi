06a4a098a701430de6dde404cfb96197bacb2552
diaa <D.abbasi@student.tudelft.nl>
2026-03-25 13:56:57 +0100

    1.1

--- sw/airborne/modules/orange_avoider/orange_avoider.h
+++ sw/airborne/modules/orange_avoider/orange_avoider.h
@@ -13,14 +13,26 @@
 #ifndef ORANGE_AVOIDER_H
 #define ORANGE_AVOIDER_H
 
-// settings
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// --- Settings (tunable via GCS if added in module xml) ---
 extern float oa_green_roi_frac_threshold;
 extern float oa_search_yaw_increment_deg;
 extern float oa_forward_step_m;
 
-// functions
+// orange detection fraction (used in merged orange_avoider.c as oa_color_count_frac)
+extern float oa_color_count_frac;
+
+// --- Module hooks ---
 extern void orange_avoider_init(void);
 extern void orange_avoider_periodic(void);
 
+#ifdef __cplusplus
+}
 #endif
+
+#endif // ORANGE_AVOIDER_H
+
 
