51b5841b41b941265aa3ac643c672677719d9fed
diaa <D.abbasi@student.tudelft.nl>
2026-03-25 14:05:53 +0100

    1.2

--- sw/airborne/modules/orange_avoider/orange_avoider.h
+++ sw/airborne/modules/orange_avoider/orange_avoider.h
@@ -9,7 +9,6 @@
  * @author Roland Meertens
  * Example on how to use the colours detected to avoid orange pole in the cyberzoo
  */
-
 #ifndef ORANGE_AVOIDER_H
 #define ORANGE_AVOIDER_H
 
@@ -17,15 +16,15 @@
 extern "C" {
 #endif
 
-// --- Settings (tunable via GCS if added in module xml) ---
+// settings (tunable via GCS if listed in the module xml)
 extern float oa_green_roi_frac_threshold;
 extern float oa_search_yaw_increment_deg;
 extern float oa_forward_step_m;
 
-// orange detection fraction (used in merged orange_avoider.c as oa_color_count_frac)
+// orange detection fraction threshold (used by merged orange_avoider.c)
 extern float oa_color_count_frac;
 
-// --- Module hooks ---
+// module hooks
 extern void orange_avoider_init(void);
 extern void orange_avoider_periodic(void);
 
@@ -34,5 +33,3 @@ extern void orange_avoider_periodic(void);
 #endif
 
 #endif // ORANGE_AVOIDER_H
-
-
